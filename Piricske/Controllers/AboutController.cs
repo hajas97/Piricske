1e464f18475151db0ba6e838986f4addd4b996a1
hajas97 <hajastomi97@gmail.com>
2023-04-11 14:47:36 +0200

    chore: create about controller

---
+++ Piricske/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Piricske.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
