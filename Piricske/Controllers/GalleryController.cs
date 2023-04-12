2fc30b8bceaf4338a8434af1c621a478ce2b36f6
hajas97 <hajastomi97@gmail.com>
2023-04-12 19:24:01 +0200

    feat: add gallery controller and view

---
+++ Piricske/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Piricske.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
