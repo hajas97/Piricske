b0b651803652ff9859c232836965ff9aa356ff21
hajas97 <hajastomi97@gmail.com>
2023-04-14 16:01:23 +0200

    feat: redesign landing page and update site.js

--- Piricske/wwwroot/js/site.js
+++ Piricske/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//mavbar elements
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+    header.classList.toggle("active");
+});
