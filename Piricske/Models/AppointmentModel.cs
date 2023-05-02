ee4805a8bfbef8a56e4e59f927beb0bdd64c4ab1
hajas97 <hajastomi97@gmail.com>
2023-05-02 11:08:16 +0200

    chore: create AppointmentModel

---
+++ Piricske/Models/AppointmentModel.cs
@@ -0,0 +1,9 @@
+﻿public class AppointmentModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
